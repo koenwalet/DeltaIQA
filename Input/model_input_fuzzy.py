deffbef3ea494d4f6d1aec0cd56741699eaa09ed
koenwalet <fencingman301@gmail.com>
2025-06-20 10:17:25 +0200

    Update model_input_fuzzy.py

--- Input/model_input_fuzzy.py
+++ Input/model_input_fuzzy.py
@@ -6,6 +6,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+
 # %%
 class CTQualityDataset:
     def __init__(self, data_dir, labels_file=None, target_size=(512, 512)):
